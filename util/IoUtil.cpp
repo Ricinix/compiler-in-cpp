9eef4e235ccd78b6a0d67794ee20ef4cc67f02c8
joestarhu <joestarhu@tencent.com>
2020-12-11 00:54:16 +0800

    feat: 初始化

---
+++ util/IoUtil.cpp
@@ -0,0 +1,5 @@
+//
+// Created by laugh on 2020/12/11.
+//
+
+#include "IoUtil.h"
---
+++ util/IoUtil.h
@@ -0,0 +1,14 @@
+//
+// Created by laugh on 2020/12/11.
+//
+
+#ifndef DESIGN_1_IOUTIL_H
+#define DESIGN_1_IOUTIL_H
+
+
+class IoUtil {
+
+};
+
+
+#endif //DESIGN_1_IOUTIL_H
