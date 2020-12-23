84090457277148f72a2be1f22c9d0ecb6e911d1e
joestarhu <joestarhu@tencent.com>
2020-12-23 19:40:57 +0800

    feat: 优化cmakelist配置

--- test.bat
+++ test.bat
@@ -1,6 +1,12 @@
 @echo off
 :: 记录下当前路径，并切换到对应路径
-pushd cmake-build-debug
+if "%1"=="release" (
+    echo testing release version
+    pushd cmake-build-release
+) else (
+    echo testing debug version
+    pushd cmake-build-debug
+)
 :: 运行程序
 design_1 -c ../data/test_1.st > ../data/test_1.log
 :: 返回刚才的路径
