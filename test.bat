d03ea53e5881b89d735f8caad325da343151a4c2
joestarhu <joestarhu@tencent.com>
2020-12-24 12:38:31 +0800

    optim: 修改输出目录

--- test.bat
+++ test.bat
@@ -2,12 +2,12 @@
 :: 记录下当前路径，并切换到对应路径
 if "%1"=="release" (
     echo testing release version
-    pushd cmake-build-release
+    pushd cmake-build-release\bin
 ) else (
     echo testing debug version
-    pushd cmake-build-debug
+    pushd cmake-build-debug\bin
 )
 :: 运行程序
-design_1 -c ../data/test_1.st > ../data/test_1.log
+design_1 -c ../../data/test_1.st > ../../data/test_1.log
 :: 返回刚才的路径
 popd
