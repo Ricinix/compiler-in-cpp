bfa9279f207ebf55d1b874dd5d1fa702cd103c57
joestarhu <joestarhu@tencent.com>
2021-03-11 10:33:18 +0800

    feat: 添加新的节点类

---
+++ ast/OpNodeElse.h
@@ -0,0 +1,14 @@
+//
+// Created by laugh on 2021/3/11.
+//
+
+#ifndef DESIGN_1_OPNODEELSE_H
+#define DESIGN_1_OPNODEELSE_H
+
+
+class OpNodeElse {
+
+};
+
+
+#endif //DESIGN_1_OPNODEELSE_H
---
+++ ast/OpNodeSimple.h
@@ -0,0 +1,14 @@
+//
+// Created by laugh on 2021/3/11.
+//
+
+#ifndef DESIGN_1_OPNODESIMPLE_H
+#define DESIGN_1_OPNODESIMPLE_H
+
+
+class OpNodeSimple {
+
+};
+
+
+#endif //DESIGN_1_OPNODESIMPLE_H
