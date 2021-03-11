5094270d36b1928cccfcafdf4dfdfea090e459ad
joestarhu <joestarhu@tencent.com>
2021-03-11 12:38:49 +0800

    feat: 在异常中打log

--- domain/exception.cpp
+++ domain/exception.cpp
@@ -4,19 +4,22 @@
 
 #include <sstream>
 #include "exception.h"
+#include "../util/Log.h"
 
-ParseException::ParseException(const Token &t) : std::logic_error(getErrorMsg("", t)) {}
+ParseException::ParseException(const Token &t) : std::logic_error(getErrorMsg("", t)) {
+    Log::error(getErrorMsg("", t));
+}
 
 std::string ParseException::getErrorMsg(const char *msg, const Token &t) {
     return "syntax error around" + location(t) + ". " + msg;
 }
 
 ParseException::ParseException(const std::string &msg, const Token &t) : std::logic_error(getErrorMsg(msg.c_str(), t)) {
-
+    Log::error(getErrorMsg(msg.c_str(), t));
 }
 
 ParseException::ParseException(const std::string &msg) : std::logic_error(msg.c_str()) {
-
+    Log::error(msg);
 }
 
 std::string ParseException::location(const Token &t) {
@@ -27,15 +30,15 @@ std::string ParseException::location(const Token &t) {
 }
 
 IoException::IoException(const std::string &msg): std::invalid_argument(msg) {
-
+    Log::error(msg);
 }
 
 IndexOutOfBoundsException::IndexOutOfBoundsException(const std::string &msg) : invalid_argument(msg) {
-
+    Log::error(msg);
 }
 
 IndexOutOfBoundsException::IndexOutOfBoundsException(int i): std::invalid_argument(generateMsg(i)) {
-
+    Log::error(generateMsg(i));
 }
 
 std::string IndexOutOfBoundsException::generateMsg(int i) {
