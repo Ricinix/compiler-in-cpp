bbc7291b6b93f602e808c56e1526529ed03be644
joestarhu <joestarhu@tencent.com>
2021-03-16 13:19:16 +0800

    optim: 优化无用代码

--- ast/CodePrinter.cpp
+++ ast/CodePrinter.cpp
@@ -4,33 +4,6 @@
 
 #include "CodePrinter.h"
 
-CodePrinter::CodePrinter() : ASTLeaf(nullptr) {
-
-}
-
-void CodePrinter::genCode(IoUtil &ioUtil) {
-    ASTLeaf::genCode(ioUtil);
-}
-
-std::string CodePrinter::toString() const {
-    return "CodePrinter";
-}
-
-std::string CodePrinter::location() const {
-    return "";
-}
-
-std::ostream &operator<<(std::ostream &os, const CodePrinter &astLeaf) {
-    os << ">>>>>>>>>>ASTLeaf>>>>>>>>>>" << std::endl;
-    os << astLeaf.toString() << std::endl;
-    os << "<<<<<<<<<<ASTLeaf<<<<<<<<<<";
-    return os;
-}
-
-ASTNodeType CodePrinter::getType() {
-    return ASTNodeType::codePrinter;
-}
-
 MethodCodePrinter::MethodCodePrinter() : DecorateNodeMethod(nullptr, false) {
 
 }
--- ast/CodePrinter.h
+++ ast/CodePrinter.h
@@ -5,27 +5,8 @@
 #ifndef DESIGN_1_CODEPRINTER_H
 #define DESIGN_1_CODEPRINTER_H
 
-#include "DefineNodeDomain.h"
 #include "DecorateNodeMethod.h"
 #include "../domain/exception.h"
-
-/**
- * 直接输出代码的叶子节点，抽象类
- */
-class CodePrinter : public ASTLeaf {
-public:
-    explicit CodePrinter();
-
-    void genCode(IoUtil &ioUtil) override;
-
-    std::string location() const override;
-
-    std::string toString() const override;
-
-    friend std::ostream &operator<<(std::ostream &os, const CodePrinter &astLeaf);
-
-    ASTNodeType getType() override;
-};
 
 class MethodCodePrinter : public DecorateNodeMethod {
 public:
